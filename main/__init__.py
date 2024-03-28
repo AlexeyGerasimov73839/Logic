1d5c1b33e7bad3cdc21e6c22a6e03a5c7b141132
AlexeyGerasimov73839 <161958050+AlexeyGerasimov73839@users.noreply.github.com>
2024-03-28 12:15:00 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("21176917", default=None, cast=int)
+API_HASH = config("6fcbda80e78e7209a661c63f23d76c7b", default=None)
+BOT_TOKEN = config("6437214223:AAE_ZJ2uu17Wu-lOkejNtvNL8c5x5qohtpE", default=None)
+SESSION = config("BAFDIlUAw6J8FoO7iICcRlGsMTgrwTdEBQdtwlqgLbqTxz9wOv8xHzysyQClTCK3A46z2z8GZ2eoFYZtwoN8F2jW2g8XmyqEhsIJc7qSrb1VKfWhjugYCwZrzzwOqyGK9lE-8ZXBI44DS-zK7piSOTAUh-7YCGfc-Gj5__l6Svs9mPuOmwZ5PyOPAPGelqACH2h9YrvdR3Rk4xXaQj2eP0klxOmrwpyvtm27XUVQvEeuwLF4s3jCyHvHmHbSP-7ngqcCiz35kCcubyg_2YDJPqIjvT1-jMRr-HNoAE-7zjSMOxHfvpSLip2Sl4gIYrieqcJhr7SfpVnbj2yRNABLCKTfud2wTwAAAAGcDxs3AA", default=None)
+FORCESUB = config("Oxicaine01", default=None)
+AUTH = config("6913202999", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
